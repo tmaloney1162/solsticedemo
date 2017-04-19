75a6dbe7eb805f193cfd4e5c3d92d017bd9fae90
tmaloney1162 <tom@themaloneygroup.com>
2017-04-19 08:53:47 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,3 +1,2 @@
-// db.js
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://crudUser:cEHad=+pew6E@ds161960.mlab.com:61960/mydb');
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-// server.js
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8500;
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
